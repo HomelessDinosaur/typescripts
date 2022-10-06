bb8a3c687ec1ed131e6decb3b91294908d2b2196
Ryan Cartwright <fryzee.cartwright@gmail.com>
2022-10-06 15:44:00 +1100

    feat: add console log

--- functions/hello.ts
+++ functions/hello.ts
@@ -4,7 +4,9 @@ const helloApi = api('main');
 
 helloApi.get("/hello/:name", async (ctx) => {
     const { name } = ctx.req.params;
-
+    
+    console.log(name);
+    
     ctx.res.body = `Hello ${name}`;
 
     return ctx;
